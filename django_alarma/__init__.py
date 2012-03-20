c04727d1ed8c687bfedd23803feeb0134817a07a
Erik van Zijst <erik.van.zijst@gmail.com>
2012-03-20 14:28:31 -0700

    Rejuggled the project layout

--- django_alarma/__init__.py
+++ django_alarma/__init__.py
@@ -22,21 +22,35 @@ class AlarmaMiddleware(object):
     def __init__(self):
         if not getattr(settings, 'ALARMA', True):
             raise MiddlewareNotUsed
+        self.orig_handler = signal.SIG_DFL
 
     def process_request(self, request):
 
-        if signal.getsignal(signal.SIGALRM) not in (handler, signal.SIG_DFL):
+        self.orig_handler = signal.getsignal(signal.SIGALRM)
+        if self.orig_handler not in (handler, signal.SIG_DFL):
             logger.warning('Your process alarm handler is already in use! '
                            'Disabling Alarma.')
             self.disabled = True
 
         if not self.disabled:
-            signal.signal(signal.SIGALRM, handler)
-            if signal.alarm(timeout):
-                logger.warning('Your process alarm handler is already in use! '
-                               'Disabling Alarma.')
+            try:
+                signal.signal(signal.SIGALRM, handler)
+            except ValueError:
+                logger.error('Unable to register SIGARLM handler; '
+                             'Alarma disabled.', exc_info=1)
+                self.disabled = True
+            else:
+                if signal.alarm(timeout):
+                    logger.warning('Your process alarm handler is already in use! '
+                                   'Disabling Alarma.')
+                    self.disabled = True
 
     def process_response(self, request, response):
         if not self.disabled:
             signal.alarm(0)
+            try:
+                signal.signal(signal.SIGALRM, self.orig_handler)
+            except ValueError:
+                logger.error('Unable to restore the previous signal.SIGALRM '
+                             'handler', exc_info=1)
         return response
